c81b4f43e772bb1487ce0665ba2693272494c2e0
yonatanlla <yonatanllanto174@gmail.com>
2023-06-15 14:28:57 -0500

    feat: subcategorias_de_subcategorias

--- Backend/src/Routes/tecnologia.js
+++ Backend/src/Routes/tecnologia.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const {postProduct} = require("../Controllers/controllersTecnologias")
-
+const {
+	getAccesorios,
+	getCelulares,
+	getConsolas,
+	getElectronica,
+	getComputacion,
+} = require("../Controllers/subCategoriasParams");
 // router.get("/productos/all",)
 // router.get("/productos/id/:id",)
 // router.get("/productos/subcategorias",) // Se busca la subcategoria por query
 
 router.post("/posteo",postProduct)
+
+// -------
+// router.get("/TV/:tv", getTV);
+router.get("/Computacion/:compu", getComputacion);
+router.get("/ElectronicaAudioVideo/:electro", getElectronica);
+router.get("/ConsolasyVideojuegos/:conso", getConsolas);
+router.get("/Celulares/:celu", getCelulares);
+router.get("/CamarasyAccesorios/:cam", getAccesorios);
+
 //rutas delete y put mas adelante
 
 
